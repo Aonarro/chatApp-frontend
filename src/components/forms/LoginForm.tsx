9ba89b586892ae23be2447db5c8e23cc292fa1ef
Nikolay Tyshchenko <nikolay.tyshchenko04@gmail.com>
2024-09-02 02:29:29 +0300

    Add some validation for register and login forms

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,22 +1,42 @@
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { Button, InputContainer, InputField, InputLabel } from '../styles'
 import s from './index.module.scss'
 
-const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-	event.preventDefault()
-}
-
 const LoginForm = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm()
+
+	console.log(errors)
+
+	const onSubmit = data => {
+		console.log(data)
+	}
 	return (
-		<form className={s.form} onSubmit={onSubmit}>
+		<form className={s.form} onSubmit={handleSubmit(onSubmit)}>
 			<InputContainer>
 				<InputLabel htmlFor='email'>Email</InputLabel>
-				<InputField type='email' id='email' />
+				<InputField
+					type='email'
+					id='email'
+					{...register('email', {
+						required: 'Email is required',
+					})}
+				/>
 			</InputContainer>
 
 			<InputContainer className={s.loginFormPasswordInput}>
 				<InputLabel htmlFor='password'>Password</InputLabel>
-				<InputField type='password' id='password' />
+				<InputField
+					type='password'
+					id='password'
+					{...register('password', {
+						required: 'Password is required',
+					})}
+				/>
 			</InputContainer>
 			<Button className={s.button}>Login</Button>
 			<div className={s.formFooter}>
--- src/components/forms/RegistrationForm.tsx
+++ src/components/forms/RegistrationForm.tsx
@@ -1,31 +1,64 @@
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { Button, InputContainer, InputField, InputLabel } from '../styles'
 import s from './index.module.scss'
 
-const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-	event.preventDefault()
-}
-
 const RegistrationForm = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm()
+
+	console.log(errors)
+
+	const onSubmit = (data: any) => {
+		console.log(data)
+	}
+
 	return (
-		<form className={s.form} onSubmit={onSubmit}>
+		<form className={s.form} onSubmit={handleSubmit(onSubmit)}>
 			<InputContainer>
 				<InputLabel htmlFor='email'>Email</InputLabel>
-				<InputField type='email' id='email' />
+				<InputField
+					type='email'
+					id='email'
+					{...register('email', {
+						required: 'Email is required',
+					})}
+				/>
 			</InputContainer>
 			<div className={s.nameFieldRow}>
 				<InputContainer>
 					<InputLabel htmlFor='firstName'>First Name</InputLabel>
-					<InputField type='text' id='firstName' />
+					<InputField
+						type='text'
+						id='firstName'
+						{...register('firstName', {
+							required: 'First name is required',
+						})}
+					/>
 				</InputContainer>
 				<InputContainer>
 					<InputLabel htmlFor='lastName'>Last Name</InputLabel>
-					<InputField type='text' id='lastName' />
+					<InputField
+						type='text'
+						id='lastName'
+						{...register('lastName', {
+							required: 'Last name is required',
+						})}
+					/>
 				</InputContainer>
 			</div>
 			<InputContainer>
 				<InputLabel htmlFor='password'>Password</InputLabel>
-				<InputField type='password' id='password' />
+				<InputField
+					type='password'
+					id='password'
+					{...register('password', {
+						required: 'Password is required',
+					})}
+				/>
 			</InputContainer>
 			<Button className={s.button}>Create a new account</Button>
 			<div className={s.formFooter}>
